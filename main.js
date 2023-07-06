af1a97635757040cd1621f799a654e5d735d49be
Mylaana <alexandre.thore34@gmail.com>
2023-07-06 14:58:59 +0200

    roll back caroussel 5

--- main.js
+++ main.js
@@ -1,14 +1,74 @@
 const urlAPI = "http://localhost:8000/api/v1/";
 const categoriesNumber = 4;
 const categories = [];
-const movieBoxPerCategory = 6;
+const movieBoxPerCategory = 5;
 
 class Category{
-    constructor(selectorIndex, title = "", id = ""){
-        this.selector = "category-" + selectorIndex + "__title";
+    defaultMovieLink = "https://caer.univ-amu.fr/wp-content/uploads/default-placeholder.png";
+    
+    constructor(selectorIndex){
+        // class references to selector
+        this.sectionSelector = document.querySelector(".category-" + selectorIndex);
+        this.titleSelector = document.querySelector(".category-" + selectorIndex + "__title");
+        this.movieContainerSelector = this.sectionSelector.querySelector('.container-movie');
+        this.caroussel = this.sectionSelector.querySelector(".caroussel");
+        
+        // buttons
+        this.buttonPrevious = this.caroussel.querySelector(".btn-previous");
+        this.buttonNext = this.caroussel.querySelector(".btn-next");
+
+        this.buttonPrevious.addEventListener("click", this.clickPrevious.bind(this));
+        this.buttonNext.addEventListener("click", this.clickNext.bind(this));
+
+        this.id = null;
+        this.title = null;
+        this.fetchResponse = null; //type : Promise
+        this.boxMovieList = [];
+    }
+    setTitle(title){
         this.title = title;
-        this.id = id;
-        this.boxMovieList = []
+        this.titleSelector.textContent = title
+    };
+    createBoxContainer(){
+        let movieBox = document.createElement("div");
+        movieBox.setAttribute("class", "box-movie");
+        this.movieContainerSelector.appendChild(movieBox);
+
+        let movieImage = document.createElement("img");
+        movieImage.setAttribute("class", "image-movie");
+        movieImage.setAttribute("src", this.defaultMovieLink);
+        movieBox.appendChild(movieImage);
+
+        let box = new boxMovie(movieBox, movieImage);
+        this.boxMovieList.push(box);
+    }
+    setFetchResult(fetchResponse){
+        this.fetchResponse = fetchResponse
+        this.displayMovieGroup(fetchResponse.results)
+    }
+    displayMovieGroup(promiseResultList){
+        for (i in this.boxMovieList){
+            let movieBox = this.boxMovieList[i];
+            if (i <= promiseResultList.length - 1){
+                movieBox.imageSelector.setAttribute("src", promiseResultList[i].image_url);
+            }
+        }
+    }
+    clickNext(){
+        if (this.fetchResponse.next != null){
+            getDataFromAPI(this.fetchResponse.next)
+            .then((response) => {
+                this.setFetchResult(response);
+            })
+        }
+    }
+    clickPrevious(){
+        if (this.fetchResponse.previous != null){
+            getDataFromAPI(this.fetchResponse.previous)
+            .then((response) => {
+                this.setFetchResult(response);
+            })
+        }
     }
 }
 class boxMovie{
@@ -34,8 +94,8 @@ function getUserFavouriteCategories(categoriesFromAPI = []){
         ];    
     }else{
         favouriteCategories.push(categoriesFromAPI[0])
-        favouriteCategories.push(categoriesFromAPI[2])
-        favouriteCategories.push(categoriesFromAPI[4])
+        favouriteCategories.push(categoriesFromAPI[1])
+        favouriteCategories.push(categoriesFromAPI[3])
     }
     return favouriteCategories
 }
@@ -46,63 +106,44 @@ function setUpCategories(categoriesListFromAPI){
         let category = new Category(i);
 
         if (i == 0){
-            category.title = "Films les mieux notés";
+            category.setTitle("Films les mieux notés");
         }else{
-            category.title = categoriesList[i-1].name;
+            category.setTitle(categoriesList[i-1].name);
             category.id = categoriesList[i-1].id;
-            console.log(`name: ${categoriesList[i-1].name}  id: ${categoriesList[i-1].id}`)
         }
-
-
-        let categoryElement = document.querySelector("." + category.selector);
-        categoryElement.textContent = category.title;
-        categoryElement.id = category.id
         categories.push(category);       
     }
 };
 
-function setUpPageStructure(){
-    // generates containers selectors for movies
-    let movieContainers = document.querySelectorAll(".container-movie");
-
-    for (let i = 0; i<=movieContainers.length -1 ; i++){
-        for (let b = 0; b<=movieBoxPerCategory -1; b++){
-            // create a new div container for the movie in category
-
-            let movieBox = document.createElement("div");
-            movieBox.setAttribute("class", "box-movie");
-            movieContainers[i].appendChild(movieBox);
-
-            let movieImage = document.createElement("img");
-            movieImage.setAttribute("class", "image-movie")
-            movieImage.setAttribute("src", "https://caer.univ-amu.fr/wp-content/uploads/default-placeholder.png")
-            movieBox.appendChild(movieImage)
-
-            let box = new boxMovie(movieBox, movieImage)
-            categories.push(box)
-        };
-    };
-}
-
 function setUpMoviesInCategory(){
     for (let i in categories){
-        if (i == 1){
+        if (i != 0){
             let category = categories[i]
-            getDataFromAPI(urlAPI + `titles/?genre=${category.title}`).then(function(response){
-                console.log(response.results)
+            getDataFromAPI(urlAPI + `titles/?genre=${category.title}`)
+            .then(function(response){
+                category.setFetchResult(response)
             })
         }
     }
 };
 
-// main script
-setUpPageStructure()
-getDataFromAPI(urlAPI + "genres/").then(function (response) {
-	// The API call was successful!
+// page initialization
+getDataFromAPI(urlAPI + "genres/")
+.then(function (response) {
 	setUpCategories(response.results);
+
+    //setup page structure
+    for (i in categories){
+        let category = categories[i];
+        for (let b = 0; b<=movieBoxPerCategory -1; b++){
+            // create a new div container for the movie in category
+            category.createBoxContainer();
+        };
+    };
     setUpMoviesInCategory();
+
 }).catch(function (err) {
 	// There was an error
-	console.log('Could not get categories from API', err);
+	console.log('Une erreur est survenue :', err);
 });
 
